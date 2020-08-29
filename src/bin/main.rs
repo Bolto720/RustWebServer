28618894b8f6ddd81d1b39539e22ad897dea81c7
Tom Bolton <bolto720@googlemail.com>
2020-08-29 19:54:54 +0100

    Added multithreading

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,13 @@
 use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
+use RustWebServer::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("192.168.1.181:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = match stream {
@@ -14,7 +18,9 @@ fn main() {
             }
         };
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -23,8 +29,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
